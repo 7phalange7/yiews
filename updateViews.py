bd644f8f46d7ca089c73e212585c0d9f2e9ca3f2
CITIZENDOT <appaji12368@gmail.com>
2021-03-27 12:20:57 +0530

    Made it deployable
    - Added Logger. Now logs will be stored in a file
    - Added requirements.txt, for creating virtualenv

--- updateViews.py
+++ updateViews.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -8,29 +10,38 @@ from time import sleep
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
+logging.basicConfig(
+    filename="Logs.log", format="%(asctime)s %(message)s", filemode="w"
+)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def main():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "auth/secret.json"  # insert
+    if len(sys.argv) < 2:
+        print("Provide Path to secret.json")
+        exit()
+    client_secrets_file = sys.argv[1]
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
+        client_secrets_file, scopes
+    )
     credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+        api_service_name, api_version, credentials=credentials
+    )
 
     vid = "U3ZsUfjdwDQ"
     count = 0
 
-    while (True):
+    while True:
         try:
-            request = youtube.videos().list(
-                part="snippet,statistics",
-                id=vid
-            )
+            request = youtube.videos().list(part="snippet,statistics", id=vid)
             response = request.execute()
 
             # print(response)
@@ -39,35 +50,36 @@ def main():
             title = video["snippet"]["title"]
             views = str(video["statistics"]["viewCount"])
             likes = str(video["statistics"]["likeCount"])
-            print("Title: ", title)
-            print("views:", views)
-            print("likes:", likes)
+            logger.info(msg=f"Title: {title}")
+            logger.info(msg=f"Views: {views}")
+            logger.info(msg=f"Likes: {likes}")
 
             changed = (views not in title) or (likes not in title)
 
-            if(changed):
-                new_title = "Avatar: Korra | Do It Like A Dude | Views: " + \
-                    views + ", Likes: " + likes
+            if changed:
+                new_title = (
+                    "Avatar: Korra | Do It Like A Dude | Views: "
+                    + views
+                    + ", Likes: "
+                    + likes
+                )
                 video["snippet"]["title"] = new_title
 
                 request = youtube.videos().update(
-                    part="snippet",
-                    body={
-                        "id": vid,
-                        "snippet": video["snippet"]
-                    }
+                    part="snippet", body={"id": vid, "snippet": video["snippet"]}
                 )
                 response = request.execute()
 
-                print("Changed")
+                logger.info(msg="Title Changed successfully")
 
-        except:
-            print("Error, try again")
+        except Exception as e:
+            logger.error(msg="Unable to Change Title")
+            logger.error(e)
             if count > 5:
                 break
 
         count += 1
-        print("\ncount: " + str(count) + "\n")
+        logger.info(msg=f"Count: {count}\n\n")
         sleep(600)
 
 
